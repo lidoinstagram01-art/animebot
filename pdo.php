37b848deeaf098aa05751d81898b2f1b623d621c
lidoinstagram01-art <lidoinstagram01@gmail.com>
2026-07-17 19:56:49 +0500

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -1,7 +1,7 @@
 <?php
 try {
     // PDO ulanishini sozlash
-    $pdo = new PDO("mysql:host=mysql.railway.internal;dbname=railway", "root", "wjTwxeGFQEddPIctRLfwphrBynQTXVVz");
+    $pdo = new PDO("mysql:host=mysql-30ecd8d8-shomurodovodilbek4-a532.h.aivencloud.com;dbname=defaultdb", "avnadmin", "AVNS_AAYSpxIB9I7WMbFWrah");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->exec("SET NAMES 'utf8mb4'");
 } catch (PDOException $e) {
